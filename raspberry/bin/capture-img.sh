ca27b4d2cc63098bad5c17a9e29780f287a0a958
hansaskov <hansaskov1999@gmail.com>
2024-04-16 15:15:22 +0200

    Updaet script to create a new directory if it does not exist

--- raspberry/bin/capture-img.sh
+++ raspberry/bin/capture-img.sh
@@ -7,5 +7,13 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+# Extract the directory path from the provided file name
+dir=$(dirname "$1")
+
+# Check if the directory exists, and create it if it doesn't
+if [ ! -d "$dir" ]; then
+    mkdir -p "$dir"
+fi
+
 # Call the rpicam-still command with the provided file name
 rpicam-still -t 0.01 -o "$1"
