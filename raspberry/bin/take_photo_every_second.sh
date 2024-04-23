5d3cfc4918c3db51e5d4facbc63b1ae8e26b0d88
hansaskov <hansaskov1999@gmail.com>
2024-04-23 14:51:30 +0200

    Update take photo every second

--- raspberry/bin/take_photo_every_second.sh
+++ raspberry/bin/take_photo_every_second.sh
@@ -1,21 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Main script
-PHOTO_DIR=~/tmp/photos
-mkdir -p "$PHOTO_DIR"
+# Set the output directory
+outdir=~/tmp/photos
 
-i=1
-while true
-do
-    filename="img$i.jpg"
-    filepath="$PHOTO_DIR/$filename"
-    if [ -f "$filepath" ]; then
-        if [ -f "$PHOTO_DIR/img$((i+1)).jpg" ]; then
-            rm "$PHOTO_DIR/img$((i+1)).jpg"
-        fi
-        mv "$filepath" "$PHOTO_DIR/img$((i+1)).jpg"
-    fi
-    rpicam-still -t 0.01 -o "$filepath"
-    i=1
-    sleep 1
+# Create the output directory if it doesn't exist
+mkdir -p "$outdir"
+
+# Define the image filenames
+img1="$outdir/img1.jpg"
+img2="$outdir/img2.jpg"
+
+while true; do
+   # Check if img1.jpg exists
+   if [ -e "$img1" ]; then
+       # Check if img2.jpg exists
+       if [ -e "$img2" ]; then
+           # Delete img2.jpg
+           rm "$img2"
+           # Rename img1.jpg to img2.jpg
+           mv "$img1" "$img2"
+       else
+           # Rename img1.jpg to img2.jpg
+           mv "$img1" "$img2"
+       fi
+   fi
+
+   # Take a new image and save it as img1.jpg
+   rpicam-still -t 0.01 -o "$img1"
+
+   # Wait for 1 second
+   sleep 1
 done
