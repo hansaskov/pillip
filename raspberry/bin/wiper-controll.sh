0a9f9373ed1873e10ec831895e255c2e3dfba376
Hans Askov <hansaskov1999@gmail.com>
2024-04-30 14:46:12 +0100

    added wipe controller

--- raspberry/bin/wiper-controll.sh
+++ raspberry/bin/wiper-controll.sh
@@ -7,13 +7,18 @@ MQTT_KEY="oliverersej"
 MQTT_TOPIC_rain="pico_write"
 MQTT_TOPIC_swipe="pico_read"
 
-#check if mqtt has recieved a "1" and take a photo 
-mosquitto_sub -t $MQTT_TOPIC_rain -h $MQTT_SERVER -p $MQTT_SERVERPORT -u $MQTT_USERNAME -P $MQTT_KEY | while read -r line; do
-    echo "read : $line"
-    jq $line
-    echo "got $?"
-    # if  [ $(jq $line ".rain_detect") == 1 ]
-    # then
-    #     echo "hej"
-    # fi
+while true
+do
+
+    json=$(mosquitto_sub -t $MQTT_TOPIC_rain -h $MQTT_SERVER -p $MQTT_SERVERPORT -u $MQTT_USERNAME -P $MQTT_KEY -C 1)
+
+    rein=$(echo "$json" | jq -r ".rain_detect")
+    if  [ "$rein" == "1" ]
+    then
+        mosquitto_pub -t $MQTT_TOPIC_swipe -h $MQTT_SERVER -p $MQTT_SERVERPORT -u $MQTT_USERNAME -P $MQTT_KEY -m "{'wiper_angle': 0}"
+        sleep 1
+        mosquitto_pub -t $MQTT_TOPIC_swipe -h $MQTT_SERVER -p $MQTT_SERVERPORT -u $MQTT_USERNAME -P $MQTT_KEY -m "{'wiper_angle': 180}"
+        sleep 1
+        mosquitto_pub -t $MQTT_TOPIC_swipe -h $MQTT_SERVER -p $MQTT_SERVERPORT -u $MQTT_USERNAME -P $MQTT_KEY -m "{'wiper_angle': 0}"
+    fi
 done
