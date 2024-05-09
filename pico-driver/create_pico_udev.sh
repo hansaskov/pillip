3427dd11e4c07b02852412b2c42446db45cd8e37
Lucas R. Jeppesen <lucasjeppesen@gmail.com>
2024-05-09 13:34:05 +0200

    move pico driver to raspberry

--- pico-driver/create_pico_udev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo tee /etc/udev/rule.d/99-pico.rules << 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000a", ATTRS{serial}=="E6605481DB404C37", MODE="0666", SYMLINK+="pico_01"'
