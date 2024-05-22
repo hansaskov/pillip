c32f32233d9e68c84b73c23bb9e2f13784485a6d
BenjaminThChristensen <103067061+BenjaminThChristensen@users.noreply.github.com>
2024-05-22 11:09:11 +0200

    Update annotate_photos.sh

--- cloud/annotate_photos.sh
+++ cloud/annotate_photos.sh
@@ -19,19 +19,15 @@ if [ -d "$dir" ]; then
         echo "$file: The key 'annotation' exists."
       else
         echo "$file: The key 'annotation' does not exist, starting annotation"
-        #use the bash script annotate.sh and take the output and save in the json file in a new key called annotation?
+        #use the bash script annotate.sh and take the output and save in the json file in a new key called annotation
         output=$(./annotate.sh ${file:0:-5}.jpg llava:13b)
         echo $output
-        #use the bash script annotate.sh and take the output and save in the json file in a new key called annotation?
         newline='{"Annotation":{"Source": "llama:13b", "Test": "$output"}}'
         
         new_content=$(jq ". += $newline" <<< cat $file)
         echo "$new_content" > "$file"
         echo "annotaion added to json file"
       fi
-   # else
-    #  echo "$file: Invalid JSON format."
-   # fi
   done
 else
   echo "The directory $dir does not exist."
