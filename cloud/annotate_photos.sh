93bd57aa4f5542598c23719a0baf03f843b8090f
hansaskov <hansaskov1999@gmail.com>
2024-05-27 16:41:06 +0200

    Fix incorrect naming of source annotation

--- cloud/annotate_photos.sh
+++ cloud/annotate_photos.sh
@@ -15,14 +15,14 @@ if [ -d "$dir" ]; then
     # Check if the file is a valid JSON file
     #if jsonlint "$file" >/dev/null 2>&1; then
       # Check if the JSON file contains the key "annotation"
-      if jq -e '. | has("annotation")' "$file" >/dev/null 2>&1; then
+      if jq -e '. | has("Annotation")' "$file" >/dev/null 2>&1; then
         echo "$file: The key 'annotation' exists."
       else
         echo "$file: The key 'annotation' does not exist, starting annotation"
         #use the bash script annotate.sh and take the output and save in the json file in a new key called annotation
         output=$(./annotate.sh ${file:0:-5}.jpg llava:13b)
         echo $output
-        newline='{"annotation":{"Source": "llama:13b", "Test": "'"$output"'"}}'
+        newline='{"Annotation":{"Source": "llava:13b", "Test": "'"$output"'"}}'
         
         new_content=$(jq ". += $newline" <<< cat $file)
         echo "$new_content" > "$file"
